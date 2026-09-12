e83d60445bb7144d114e36fa8404255be671af9a
Zhaqian Rouf Alfauzi <zhaqianroufa@gmail.com>
2026-09-12 15:01:25 +0700

    add : a New

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -11,10 +11,7 @@ class Event extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
-protected $fillable = [
-    // ... kolom lainnya ...
-    'status_absen',
-];
+
     // Auto generate UUID saat membuat event baru
     protected static function boot()
     {
